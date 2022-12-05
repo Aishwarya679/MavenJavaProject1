c5740e7e7c76040705a1d7a21dfe5756a984fcbb
Aishwarya679 <kamatapuaishwarya@gmail.com>
2022-12-05 21:59:30 +0530

    First commit to maven java project

---
+++ src/main/java/SEGroup/MavenJavaProject_79/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaProject_79;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from Aishwarya-20BD1A6679" );
+    }
+}
